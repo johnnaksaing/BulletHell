cfbb87506113916385c253785ba33b536e5e16ef
johnnaksaing <johnnaksaing@daum.net>
2022-09-09 02:11:41 +0900

    Add AstroidLine
    알고리즘 참고해서 추가할 것.
    .sln 솔루션 이름 바꾸고 싶다..

--- osc/AsteroidLine.cpp
+++ osc/AsteroidLine.cpp
@@ -2,12 +2,37 @@
 #include "AsteroidLine.h"
 #include "Game.h"
 
-AsteroidLine::AsteroidLine(class Game* game) : Actor(game), m_LineCount(0)
+AsteroidLine::AsteroidLine(class Game* game, size_t LineCount, Vector2 initialPosition, float initalRotation, Vector2 endPosition, float endRotation)
+	: Actor(game),
+	m_LineCount(LineCount),
+	m_Position(initialPosition),
+	m_Rotation(initalRotation),
+	m_EndPosition(endPosition),
+	m_EndRotation(endRotation)
 {
-	m_Asteroids.resize(m_LineCount);
+	m_Astroids.resize(LineCount);
 }
 
 AsteroidLine::~AsteroidLine()
 {
 	GetGame()->RemoveActor(this);
+}
+
+void AsteroidLine::UpdateActor(float deltaTime) 
+{
+	Actor::UpdateActor(deltaTime);
+
+	/*
+	//근데 이거 맞음?
+	if (m_Astroids.size() == m_LineCount)
+		return;
+
+	// Is it time to spawn a new enemy?
+	mNextEnemy -= deltaTime;
+	if (mNextEnemy <= 0.0f)
+	{
+		new Enemy(GetGame());
+		mNextEnemy += EnemyTime;
+	}
+	*/
 }
--- osc/AsteroidLine.h
+++ osc/AsteroidLine.h
@@ -6,13 +6,24 @@
 class AsteroidLine : public Actor
 {
 public:
-	AsteroidLine(class Game* game);
+	AsteroidLine(class Game* game, size_t LineCount, Vector2 initialPosition, float initalRotation, Vector2 endPosition, float endRotation);
 	~AsteroidLine();
 	
-	void AddAsteroid(class Asteroid* atd) { m_Asteroids.push_back(atd); }
-	void UpdateActor(float deltaTime) override {};
+	//void AddAsteroid(class Asteroid* atd) { m_Asteroids.push_back(atd); }
+	void UpdateActor(float deltaTime) override;
 
 private:
-	std::vector<class Asteroid*> m_Asteroids;
 	size_t m_LineCount;
+	std::vector<class Astroid*> m_Astroids;
+
+	//depends on first one..?
+	Vector2 m_Position;
+	float m_Rotation;
+
+	//destination
+	Vector2 m_EndPosition;
+	float m_EndRotation;
+
+	//interbal between line members(deltaTime)
+	float m_LerpSpeed;
 };
