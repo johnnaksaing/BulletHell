8af178d4cc5c61fc49a3cd5cf1eaf2e53cb677e8
johnnaksaing <johnnaksaing@daum.net>
2022-09-30 17:49:05 +0900

    update level code
    still WIP

--- osc/Source/Level.cpp
+++ osc/Source/Level.cpp
@@ -1,11 +1,15 @@
 #pragma once
 #include "Level.h"
+#include "Game.h"
 #include <vector>
+#include <algorithm>
 Level::Level(Game* game) :
 	m_Game(game),
-	m_Tiles(nullptr)
+	m_Tiles(nullptr),
+	m_spwnerIdx(0)
 {
-
+	//테스트 시 여기에 spwanwer 등록
+	std::sort(m_ActorSpawner.begin(), m_ActorSpawner.end());
 }
 
 Level::~Level() 
@@ -15,5 +19,11 @@ Level::~Level()
 
 void Level::Update(float deltaTime) 
 {
-
+	m_LevelTimer += deltaTime;
+	while (m_LevelTimer < m_ActorSpawner[m_spwnerIdx].first)
+	{
+		//이거 작동안하는 의사코드임. 여기서 new로 생성시켜줘야 한다.
+		m_Game->AddActor(m_ActorSpawner[m_spwnerIdx].second);
+		m_spwnerIdx++;
+	}
 }
--- osc/Source/Level.h
+++ osc/Source/Level.h
@@ -4,13 +4,16 @@
 
 // 이거 그냥 game 상속받아서 클래스 리스트만 만들면 되나?
 // https://github.com/EpicGames/UnrealEngine/blob/release/Engine/Source/Runtime/Engine/Private/Level.cpp
+
+// XMLize level
+// https://www.youtube.com/watch?v=wEXuNNKG9xw&ab_channel=AtherOmar
 class Level 
 {
 public:
 	Level(class Game* game);
 	~Level();
 
-	void AddActor(class Actor* AddingActor);
+	//virtual void AddActor(class Actor* AddingActor) = 0;
 	std::vector<class Actor*> GetActors() const { return m_Actors; };
 	void Update(float deltaTime);
 private:
@@ -18,8 +21,17 @@ private:
 
 	std::vector<class Actor*> m_Actors;
 	std::vector<std::pair<float, class Actor*>> m_ActorSpawner;
-
+	size_t m_spwnerIdx;
 	Vector2 m_playerStart;
 
-	class Tile** m_Tiles;
+	std::vector<class BGActor*> m_background;
+
+	int stageCount;
+	
+	class Player* m_PlayerCharacter;
+
+	float m_LevelTimer;
+	float m_spawnTimerMax;
+	
+	class XML* m_RawData;
 };
